4fcf571eaaab4a6f0fba52b9a67deba448312aaa
Rob deMallac <demallac@gmail.com>
2017-01-05 13:14:08 -0800

    Update simplewebApplication.java
    for no other reason than to trigger a Jenkins build

--- src/main/java/com/grs/SimplewebApplication.java
+++ src/main/java/com/grs/SimplewebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplewebApplication {
 
+	// This is a comment to trigger a Jenkins build
 	public static void main(String[] args) {
 		SpringApplication.run(SimplewebApplication.class, args);
 	}
